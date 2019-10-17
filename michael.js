80914d11fc73290596ce6718bb238b38f24d4fce
mickaelreil <michael.reilhac@live.fr>
2019-10-17 13:30:10 +0200

    MICKAEL PAGE

--- michael.js
+++ michael.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
     var i=0;
     var sleep=500;
+    var ip=get_ip(set_cookie);
+    alert($.cookie('ip'));
+    //$("user",Math.random()%20);
+    $("#myModal").css("display","block");
+    $(window).on("click",function(){
+	$("#myModal").css("display","none");
+    });
     for(i=1;i<=100;i++){
 	set_bar(i,sleep);
     }
     
 
 });
+
 function set_bar(i,sleep){
     setTimeout(function(){
 	$(".progress-bar").css("width",i+"%");
-	$("h2").text("loading : "+i+"%")
+	$("h2").text(i+"%")
     },i*sleep);
+}
+function get_ip(callback){
+    $.getJSON("https://api.ipify.org/?format=json", function(e) {
+        callback(e.ip);
+    });
+
+}
+
+function set_cookie(ip){
+    $.cookie('ip',ip);
 }
