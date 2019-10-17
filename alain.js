c0b375ab7aacdd4c2d8b2c4f27a77108e3c78ede
Alain1798 <alain201017@hotmail.fr>
2019-10-17 14:26:29 +0200

    update

--- alain.js
+++ alain.js
@@ -1,8 +1,19 @@
-let para = document.getElementById(player);
+let para = document.querySelector('nom');
+let para2 = document.querySelector('prenom');
 
 para.addEventListener('click', updateName);
+para2.addEventListener('click', updateFirstName);
 
 function updateName() {
-  let name = prompt('Enter a new name');
-  para.textContent = 'Player 1: ' + name;
+  let name = prompt('Entrez le prenom');
+  para.textContent = 'Nom : ' + name;
+}
+
+function updateFirstName() {
+  let name = prompt('Entrez le prenom ');
+  para.textContent = 'Prenom : ' + name;
+}
+
+function begin(){
+	  alert("QUE LE COMBAT COMMENCE !!!!");
 }
